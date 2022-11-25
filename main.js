474a3769b0ac8f30b5ff343260096331c2435da5
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-11-25 14:22:06 +0200

    init

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function fadeIn(e) {
+    e.classList.remove('hidden', 'fadeOut')
+    e.classList.add('fadeIn')
+}
+
+function fadeOut(e) {
+    e.classList.remove('hidden', 'fadeIn')
+    e.classList.add('fadeOut')
+}
+
+function hidden(e) {
+    e.classList.remove('fadeOut', 'fadeIn')
+    e.classList.add('hidden')
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     for (var i = pairs.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -7,25 +22,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     var i = 0
-    const img = document.getElementById('img')
-
-    const [src_o, _] = pairs[0]
-    img.src = src_o
-
+    const img_o = document.getElementById('img_o')
+    const img_i = document.getElementById('img_i')
 
     const progress = document.getElementById('progress')
     btns = document.querySelectorAll('.btn')
     function nextImg() {
         btns.forEach(btn => btn.setAttribute('disabled', 'true'))
-        const [_, src_i] = pairs[i++]
-        img.src = src_i
         progress.classList.add('slide')
+        fadeIn(img_i)
+        fadeOut(img_o)
         setTimeout(() => {
-            const [src_o, _] = pairs[i]
-            img.src = src_o
+            const [src_o, src_i] = pairs[i++]
+            img_o.src = src_o
+            img_i.src = src_i
+            fadeIn(img_o)
+            hidden(img_i)
             btns.forEach(btn => btn.removeAttribute('disabled'))
             progress.classList.remove('slide')
-        }, 5000)
+        }, 4000)
     }
+    nextImg()
     btns.forEach(btn => btn.addEventListener('click', nextImg))
 })
