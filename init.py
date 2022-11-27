485e2c51b51f1414b406c9f66ebcb839d6e1d146
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-11-27 20:29:24 +0200

    imgs

--- init.py
+++ init.py
@@ -1,12 +1,25 @@
 import json
+import re
 from pathlib import Path
 
-disney = Path('disney')
-pairs = [
-    [(disney / 'output' / jpg.name).with_suffix('.d.jpg'), jpg]
-    for jpg in (disney / 'input').glob('*.jpg')]
+import requests
+from tqdm import tqdm
 
-pairs = [[str(o), str(i)] for o, i in pairs if o.exists() and i.exists()]
 
-with open('data.js', 'w') as f:
-    f.write('pairs = ' + json.dumps(pairs))
+def imgs2data():
+    imgs = Path('imgs')
+    pairs = [
+        [img.with_suffix('.jpeg'), img]
+        for img in imgs.rglob('*.jpg')]
+
+    for i, o in pairs:
+        assert i.exists() and o.exists()
+
+    pairs = [[str(o), str(i)] for o, i in pairs]
+
+    with open('data.js', 'w') as f:
+        f.write('pairs = ' + json.dumps(pairs))
+
+
+if __name__ == '__main__':
+    imgs2data()
--- main.js
+++ main.js
@@ -29,7 +29,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const progress = document.getElementById('progress')
     btns = document.querySelectorAll('.btn')
+    // const myConfetti = confetti.create(document.getElementById('confetti'), {
+    //     useWorker: true
+    // });
     function nextImg() {
+        confetti()
+        // myConfetti.reset()
+        // myConfetti({
+        //     particleCount: 100,
+        //     spread: 160
+        // });
         btns.forEach(btn => btn.setAttribute('disabled', 'true'))
         progress.classList.add('slide')
         fadeIn(img_i)
